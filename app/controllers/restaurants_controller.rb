92ececacbbedf43c3686cd296f2005537b2e8868
Ivan Sued <thefenry@gmail.com>
2015-04-16 13:59:44 -0700

    Update tabbing that is messing up on file upload

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,8 +2,8 @@ class RestaurantsController < ApplicationController
   
   def index
 	@restaurant_list =  Restaurant.all
-	respond_to do |format|
-		format.json { render json: @restaurant_list.as_json(only: [:id, :name]) }
+		respond_to do |format|
+			format.json { render json: @restaurant_list.as_json(only: [:id, :name]) }
   		format.xml { render xml: @restaurant_list.to_xml(skip_types: true, only: [:id, :name]) }
     end
   end
