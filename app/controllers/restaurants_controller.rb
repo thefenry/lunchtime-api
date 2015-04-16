2dba0452a329fc1b28424a23991174f88f043a9e
Ivan <thefenry@gmail.com>
2015-04-16 13:54:34 -0700

    Fix styling issue

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,10 +2,10 @@ class RestaurantsController < ApplicationController
   
   def index
 	@restaurant_list =  Restaurant.all
-  	  respond_to do |format|
-        format.json { render json: @restaurant_list.as_json(only: [:id, :name]) }
+	respond_to do |format|
+		format.json { render json: @restaurant_list.as_json(only: [:id, :name]) }
   		format.xml { render xml: @restaurant_list.to_xml(skip_types: true, only: [:id, :name]) }
-      end
+    end
   end
 
 end
